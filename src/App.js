635acdffa3e0c1804aaf185c6267b6513e821641
priyanshuRj <priyanshurajput0071109@gmail.com>
2023-01-21 19:46:25 +0530

    Started School

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import {
   Route,
 } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
-import Dashboard from "./components/Student/Dashboard";
+
+// Student Pages Imports
+import StudentDashboard from "./components/Student/Dashboard";
 import StudentSubjects from "./components/Student/Subjects";
 import StudentTest from './components/Student/Test';
 import StudentFees from './components/Student/FeesPayment';
+
+// School Pages Imports
+import SchoolDashboard from "./components/School/Dashboard";
 import "./App.css";
 export default function App() {
   return (
@@ -18,10 +23,16 @@ export default function App() {
 
      
         <Routes>
+
+          {/* Students links  */}
+        <Route path='/student/dashboard' element={<StudentDashboard/>} />
         <Route path='/student/subject' element={<StudentSubjects/>} />
-        <Route path='/student/dashboard' element={<Dashboard/>} />
         <Route path='/student/test' element={<StudentTest/>} />
         <Route path='/student/fees' element={<StudentFees/>} />
+
+        {/* School Links */}
+        <Route path='/school/dashboard' element={<SchoolDashboard/>} />
+
         <Route path='/' element={<LandingPage/>} />
         </Routes>
       </div>
